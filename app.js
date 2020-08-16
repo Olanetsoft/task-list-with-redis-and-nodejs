3eccdc36a5a9bbbe2da7c3b94b867f2f045b3603
olanetsoft <heedris2olubisi@gmail.com>
2020-08-16 14:52:33 +0100

    redis client setup completed and task retrieved

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const redis = require('redis');
 
 const app = express();
 
+// Create Redis client
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+    console.log('Redis Server Connected.. ')
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -13,9 +21,9 @@ app.set('view engine', 'ejs');
 
 // This line below is required to parse the request body
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
- 
+
 
 // Global Middleware registered
 // Using morgan only in development
@@ -23,8 +31,17 @@ if (process.env.NODE_ENV === 'development') {
     app.use(logger('dev'));
 };
 
-app.get('/', (req, res)=>{
-    res.send("welcome to home page")
+app.get('/', (req, res) => {
+    const title = 'Task List';
+
+    client.lrange('Tasks', 0, -1, (err, reply) => {
+        res.render('index', {
+            title,
+            tasks: reply
+        });
+    });
+
+
 })
 
 
@@ -33,5 +50,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}...`);
 });
- 
+
 module.exports = app;
