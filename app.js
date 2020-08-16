1479a3b58561b017e73ae4b7fca91545a52306e3
olanetsoft <heedris2olubisi@gmail.com>
2020-08-16 14:34:39 +0100

    server setup

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const redis = require('redis');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+// This line below is required to parse the request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+
+// Global Middleware registered
+// Using morgan only in development
+if (process.env.NODE_ENV === 'development') {
+    app.use(logger('dev'));
+};
+
+app.get('/', (req, res)=>{
+    res.send("welcome to home page")
+})
+
+
+// Port config 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}...`);
+});
+ 
+module.exports = app;
